0f0cb9ba890135c8cd6cdfe2ef8d1376e61a013e
Chealsea Co Tan <chealseaco.tan@my.jcu.edu.au>
2024-10-21 00:12:56 +0800

    Add Back button and its functionality for Difficulty, Help and Questions Scenes

--- Assets/SceneLoader.cs
+++ Assets/SceneLoader.cs
@@ -5,21 +5,71 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    private static string lastScene; // Saves the last scene used/opened for back button
+
     public void LoadARScene()
     {
+        StoreLastScene();
         Debug.Log("Begin button pressed!");
         SceneManager.LoadScene("SampleScene");
     }
 
     public void LoadQuizScene()
     {
+        StoreLastScene();
         Debug.Log("Quiz button pressed!");
         SceneManager.LoadScene("DifficultyScene");
     }
 
     public void LoadHelpGuideScene()
     {
+        StoreLastScene();
         Debug.Log("Help button pressed!");
         SceneManager.LoadScene("HelpGuideScene");
+    }
+
+    public void LoadEasyScene()
+    {
+        Debug.Log("Easy button pressed!");
+        SceneManager.LoadScene("QuizQuestionScene");
+    }
+
+    public void LoadNormalScene()
+    {
+        Debug.Log("Normal button pressed!");
+        SceneManager.LoadScene("QuizQuestionScene");
+    }
+
+    public void LoadHardScene()
+    {
+        Debug.Log("Hard button pressed!");
+        SceneManager.LoadScene("QuizQuestionScene");
+    }
+
+    private void StoreLastScene()
+    {
+        lastScene = SceneManager.GetActiveScene().name; // Get the current active scene name
+    }
+
+    // Function for the Back button
+    public void GoBack()
+    {
+        if (!string.IsNullOrEmpty(lastScene))
+        {
+            SceneManager.LoadScene(lastScene); // Load the last scene
+        }
+        else
+        {
+            Debug.LogWarning("No last scene to go back to.");
+        }
+    }
+
+    private void Update()
+    {
+        // Check if the Android back button is pressed
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            GoBack(); 
+        }
     }
 }
