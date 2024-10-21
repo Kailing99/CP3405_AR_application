2a5bd5ca91ab795a7aa6b4ae580cde5f5aa6a142
Theodore Lee <qitheodore.lee@my.jcu.edu.au>
2024-10-21 23:35:02 +0800

    Add initial code for planet interaction.

--- Assets/PlanetInteraction.cs
+++ Assets/PlanetInteraction.cs
@@ -1,18 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlanetInteraction : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject infoPanel;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.CompareTag("Planet"))
+                {
+                    ShowPlanetInfo(hit.transform.gameObject);
+                }
+            }
+        }
+    }
+
+    void ShowPlanetInfo(GameObject planet)
+    {
+        infoPanel.SetActive(true);
+        infoPanel.GetComponentInChildren<UnityEngine.UI.Text>().text = "Info about " + planet.name;
+    }
+
+    public void CloseInfoPanel()
+    {
+        infoPanel.SetActive(false);
     }
 }
